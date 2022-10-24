4ba8b4cafcead83ac05eb7389fc77e785160c785
someCodeMaker <denishudzenko1@gmail.com>
2022-10-24 04:08:51 +0300

    add login|sign pages

--- js/app.js
+++ js/app.js
@@ -36,3 +36,37 @@ function footerMenu() {
   })
 }
 footerMenu();
+
+//? Login|Sign-in button
+
+function authorizationBtn() {
+  const login = document.getElementById("login");
+  const sign = document.getElementById("sing-in");
+  const closeBtn = document.querySelector(".close-btn")
+  const loginOpen = document.querySelector(".login-close");
+  const signOpen = document.querySelector(".sign-close");
+  const closeSign = document.querySelector(".close-sign")
+
+  login.addEventListener("click", () => {
+    loginOpen.classList.add("login-open");
+    loginOpen.classList.remove("login-close");
+    if (loginOpen) {
+      closeBtn.addEventListener("click", () => {
+        loginOpen.classList.add("login-close")
+        loginOpen.classList.remove("login-open")
+      })
+    }
+  })
+
+  sign.addEventListener("click", () => {
+    signOpen.classList.add("sign-open");
+    signOpen.classList.remove("sign-close");
+
+    closeSign.addEventListener("click", () => {
+      signOpen.classList.add("sign-close");
+      signOpen.classList.remove("sign-open");
+    })
+  })
+}
+
+authorizationBtn();
