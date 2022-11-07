ff8ee6f498f09e5997c9f4b64ff617a37d6343fc
someCodeMaker <denishudzenko1@gmail.com>
2022-11-07 19:10:47 +0200

    add reviews and review slider

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ import { initContent, addContent, fragmentTemplate } from "./init_content.js"
 
 initContent(plants, ".plants-list", templates.plant); //! initial html markup plant in plants-list 
 
+
 //! burger menu
 //__________________________
 function burger() {
@@ -31,14 +32,30 @@ burger();
 
 function footerMenu() {
   const menu = document.querySelectorAll(".footer-list");
-  const menuBtn = document.querySelectorAll(".footer-right__title");
-  const menuContainer = document.querySelectorAll(".footer-right__wrapper")
+  const menuBtn = document.querySelectorAll(".footer-title__wrapper");
+  const menuContainer = document.querySelectorAll(".footer-right__wrapper");
+  const openArrow = document.querySelectorAll(".open-arrow")
 
 
   menuContainer.forEach((item, index) => {
-    menuBtn[index].addEventListener("click", (e) => {
-      menu[index].classList.toggle("show")
-      menuContainer[index].classList.toggle("footer-menu-open")
+    menuBtn[index].addEventListener("click", e => {
+      if (menu[index].classList.contains("show")) {
+        openArrow[index].classList.remove("arrow-reverse")
+        menu[index].classList.remove("show");
+        e.currentTarget.parentElement.classList.remove("footer-menu-open");
+
+      } else {
+
+        menu.forEach((item, index) => {
+          item.classList.remove("show");
+          menuContainer[index].classList.remove("footer-menu-open")
+          openArrow[index].classList.remove("arrow-reverse")
+        });
+
+        openArrow[index].classList.add("arrow-reverse")
+        menu[index].classList.add("show");
+        menuContainer[index].classList.add("footer-menu-open");
+      }
     })
   })
 }
@@ -81,6 +98,7 @@ authorizationBtn();
 
 
 //! Hover item question icon
+//________________________________
 function hoverInfo() {
 
   const itemInfo = document.querySelectorAll(".item-question");
@@ -100,6 +118,66 @@ function hoverInfo() {
 
 hoverInfo();
 
+//! Review initialization and slider realization
+// _________________________________________________
+function reviewPaginationControl(arr) {
+  const paginationPrevious = document.querySelector("#pagination-left")
+  const paginationNext = document.querySelector("#pagination-right")
+  const reviewWrapper = document.querySelector(".review-card__wrapper")
+  let review = 0
+
+  initReview(arr, 0, reviewWrapper, templates.review);//init a first review on page
+
+  // initialization review html markup to page
+  function initReview(arr, index, id, contentFunctionName) {
+
+    const fragment = document.createDocumentFragment();
+    fragment.append(contentFunctionName(arr[index]));
+
+    addReview(fragment, id);
+  }
+
+  // add html code to .review-card__wrapper block
+  function addReview(content, element) {
+    element.innerHTML = ""
+    element.append(content);
+  }
+
+  // next review
+  function nextReview() {
+    review++;
+    if (review >= arr.length) {
+      review = 0;
+    } else if (review < 0) {
+      review = arr.length - 1;
+    }
+
+    initReview(arr, review, reviewWrapper, templates.review);
+
+  }
+
+  // previous review
+  function previousReview() {
+    review--
+    if (review >= arr.length) {
+      review = 0;
+    } else if (review < 0) {
+      review = arr.length - 1
+    }
+
+    initReview(arr, review, reviewWrapper, templates.review);
+
+  }
 
 
-// initContent(reviews, ".review-card__wrapper", templates.review)//! initial html markup reviews in plants-list
+
+  paginationPrevious.addEventListener("click", previousReview);//previous review button
+  paginationNext.addEventListener("click", nextReview);// next review button
+
+}
+
+reviewPaginationControl(reviews);
+
+
+
+
